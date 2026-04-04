59f69e39c6cd48380ba531328899cf49704db146
Your Name <you@example.com>
2026-04-04 15:19:41 +0400

    detailed-search finish

---
+++ src/Components/AboutSection/AboutSection.tsx
@@ -0,0 +1,10 @@
+
+export default function AboutSection() {
+  return (
+    <div className="container">
+        <div className="about-section">
+            
+        </div>
+    </div>
+  )
+}
---
+++ src/Components/AboutSection/AboutSectionText.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutSectionText() {
+  return (
+    <div>AboutSectionText</div>
+  )
+}
